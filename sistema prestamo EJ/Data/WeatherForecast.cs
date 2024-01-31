f66a29aca19063c6a5721bdfe24ff1f959b2c5fa
josiasduran <157169421+josiasduran@users.noreply.github.com>
2024-01-31 14:11:28 -0400

    Agregar archivos de proyecto.

---
+++ sistema prestamo EJ/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace sistema_prestamo_EJ.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
